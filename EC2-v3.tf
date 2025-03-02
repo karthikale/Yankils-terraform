efb1de69e3e0f97c9d6ca4ebb3cc5ea5ef3fda8e
karthik ale <karthikale518@gmail.com>
2025-03-02 12:35:56 -0800

    ec2- v2 file

--- EC2-v3.tf
+++ EC2-v3.tf
@@ -6,7 +6,9 @@ resource "aws_instance" "studio-server"{
     ami = "ami-094b981da55429bfc"
     instance_type = "t2.micro"
     key_name = "Jenkins_Server_Keypair"
-    security_groups = ["studio-server-sg"]
+    //security_groups = ["studio-server-sg"]
+    vpc_security_group_ids = [aws_security_group.studio-server-sg.id]
+    subnet_id = aws_subnet.studio-public-subnet-01.id  
 }
 
 resource "aws_security_group" "studio-server-sg"{
@@ -72,4 +74,13 @@ resource "route_table" "studio-public-route-table"{
     }   
 }
 
-resource "aws_route_table_association" "studio-"
+resource "aws_route_table_association" "studio-public-rt-association-01"{
+    subnet_id = aws_subnet.studio-public-subnet-01.id
+    route_table_id = route_table.studio-public-route-table.id   
+}
+
+resource "aws_route_table_association" "studio-public-rt-association-02"{
+    subnet_id = aws_subnet.studio-public-subnet-02.id
+    route_table_id = route_table.studio-public-route-table.id
+}
+   
